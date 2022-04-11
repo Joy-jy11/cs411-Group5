746af0d1f970c3d30eda6ae5e7204748ef13abd0
Yiw_C <oncwnuNGQeRLsDB1096jZZmGsDN8@git.weixin.qq.com>
2022-04-10 23:29:49 -0400

    recreating the frontend folder

--- mysite/frontend/src/App.js
+++ mysite/frontend/src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import axios from "axios";
 
 function App() {
-
   return (
-
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="http://127.0.0.1:8000/app"
           target="_blank"
           rel="noopener noreferrer"
-
         >
-          Click Here
-        </a >
+          Learn React
+        </a>
       </header>
     </div>
   );
