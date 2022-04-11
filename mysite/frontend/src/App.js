a384ef6e636ab08a4094da2d4db8b9424cfdaf96
Yiw_C <oncwnuNGQeRLsDB1096jZZmGsDN8@git.weixin.qq.com>
2022-04-11 00:46:12 -0400

    search.js added to create the search box

--- mysite/frontend/src/App.js
+++ mysite/frontend/src/App.js
@@ -1,14 +1,22 @@
+
 import logo from './logo.svg';
 import './App.css';
+import Search from './search';
+
+
+
+//Deleted the initial display content 
+//<p>
+//Edit <code>src/App.js</code> and save to reload.
+//</p>
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="logo" />       
+        <Search /> 
         <a
           className="App-link"
           href="http://127.0.0.1:8000/app"
